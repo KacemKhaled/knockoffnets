98e8afe07e19bb1489eaa731457e1800a4e8f013
kacem <kcm.khaled@gmail.com>
2021-01-11 19:22:41 -0500

    imagenet edits

--- train-script.sh
+++ train-script.sh
@@ -25,7 +25,7 @@ cd ..
 
 # Prepare data
 mkdir $SLURM_TMPDIR/ILSVRC2012
-tar xf ~/scratch/kacem/datasets/ILSVRC2012/ILSVRC2012_img_train.tar -C $SLURM_TMPDIR/ILSVRC2012
+tar -xvf ~/scratch/kacem/datasets/ILSVRC2012/ILSVRC2012_img_train.tar -C $SLURM_TMPDIR/ILSVRC2012
 
 cd $SLURM_TMPDIR/ILSVRC2012
 find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xvf "${NAME}" -C "${NAME%.tar}"; rm -f "${NAME}"; done
@@ -44,4 +44,4 @@ python -m "knockoff.adversary.transfer" random $SOURCEDIR/models/victim/fashionm
         --out_dir $SOURCEDIR/models/adversary/fashionmnist-lenet-random --budget 5000 \\
         --queryset ImageNet1k --batch_size 8 -d 0
 
-#launch the script with: sbatch --gres=gpu:1 --cpus-per-task=6 --mem=32000M --time=0-01:00 train-script.sh
+#launch the script with: sbatch --gres=gpu:1 --cpus-per-task=6 --mem=32000M --time=0-00:30 train-script.sh

c9bca5b194213b8eb80da194ff47dd6b84140acb
kacem <kcm.khaled@gmail.com>
2021-01-13 00:00:58 -0500

    imagenet updates

--- train-script.sh
+++ train-script.sh
@@ -25,8 +25,8 @@ mkdir $SLURM_TMPDIR/ILSVRC2012
 tar -xvf ~/scratch/kacem/datasets/ILSVRC2012/ILSVRC2012_img_train.tar -C $SLURM_TMPDIR/ILSVRC2012
 #tar -xvf ~/scratch/kacem/datasets/ILSVRC2012/ILSVRC2012_img_train_t3.tar -C ~/scratch/kacem/datasets/train3
 
-#cd ~/scratch/kacem/datasets/train3
-#find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xvf "${NAME}" -C "${NAME%.tar}"; rm -f "${NAME}"; done
+cd $SLURM_TMPDIR/ILSVRC2012
+find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xvf "${NAME}" -C "${NAME%.tar}"; rm -f "${NAME}"; done
 
 #mkdir $SLURM_TMPDIR/data
 #tar xf ~/projects/def-xxxx/data.tar -C $SLURM_TMPDIR/data
